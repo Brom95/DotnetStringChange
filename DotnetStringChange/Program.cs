b1acb6130a30ed051e0ac8b36cd5c3527810480f
wasal <wasal@GAMESTATION>
2022-12-24 09:45:45 +0300

    Добавьте файлы проекта.

---
+++ DotnetStringChange/Program.cs
@@ -0,0 +1,16 @@
+﻿
+using System.Runtime.InteropServices;
+
+var a = "Hello, World!";
+Console.WriteLine("Переменная a:");
+Console.WriteLine(a);
+Console.WriteLine("Прямой вывод \"Hello, World!\"");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("\nПроизводим магию...\n");
+
+MemoryMarshal.CreateSpan(ref MemoryMarshal.GetReference(a.AsSpan()), a.Length)[0] = 'T';
+
+Console.WriteLine("Переменная a:");
+Console.WriteLine(a);
+Console.WriteLine("Прямой вывод \"Hello, World!\""); // Tello, World!
+Console.WriteLine("Hello, World!"); // Tello, World!
---
+++ UnsafeStringChange/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices;
+
+var a = "Hello, World!";
+Console.WriteLine("Переменная a:");
+Console.WriteLine(a);
+Console.WriteLine("Прямой вывод \"Hello, World!\"");
+Console.WriteLine("Hello, World!");
+Console.WriteLine("\nПроизводим магию...\n");
+
+unsafe
+{
+    fixed(char* c = a)
+    {
+        *c = 'T';
+    }
+}
+Console.WriteLine("Переменная a:");
+Console.WriteLine(a);
+Console.WriteLine("Прямой вывод \"Hello, World!\""); // Tello, World!
+Console.WriteLine("Hello, World!"); // Tello, World!
